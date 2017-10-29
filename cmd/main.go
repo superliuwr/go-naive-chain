fdefc0a2306988ffe968ff3bc615c5b7b5dbe07c
Marvin Liu <superliuwr@gmail.com>
2017-10-29 20:23:34 +1100

    Fix compilation error

--- cmd/main.go
+++ cmd/main.go
@@ -3,24 +3,24 @@ package main
 import (
 	"flag"
 	"log"
-	"strings"
 
 	"github.com/superliuwr/go-naive-chain/lib/service"
+	"github.com/superliuwr/go-naive-chain/lib/util"	
 	"github.com/superliuwr/go-naive-chain/lib/web"
 )
 
 func main() {
 	webServerPort := flag.String("port", "8080", "web server port number")
+	nodeID := flag.String("id", util.PseudoUUID(), "node ID")
 	flag.Parse()
 
 	// Step 1 Create a blockchain and initialize genesis block
 	blockchain := service.NewBlockchain()
-	nodeID := strings.Replace(util.PseudoUUID(), "-", "", -1)
 
 	// Step 2 Setup peers connection
 	// Step 3 Start P2P server
 	// Step 4 Start HTTP server
-	httpServer := web.NewServer(webServerPort, blockchain, nodeID)
+	httpServer := web.NewServer(webServerPort, blockchain, *nodeID)
 	err := httpServer.Start()
 	if err != nil {
 		log.Fatal("Unable to start Web server", err)
